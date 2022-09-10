ad8b59531d2b4a0612241fc5121dc757ec242baa
EsauProgramando <eaveintemillap@alumno.unsm.edu.pe>
2022-09-10 17:09:37 -0500

    nuevo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import Home from "./pages/Home";
 
-import Login from "./pages/Login/Login";
-import NavigationWrapper from "./components/NavigationWrapper/NavigationWrapper";
-import Nomatch from "./components/NoMatch/Nomatch";
+import Login from "./pages/Login";
+import NavigationWrapper from "./components/NavigationWrapper";
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
           <Route exact path="/" element={<Home />} />
         </Route>
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Nomatch />} />
       </Routes>
     </Router>
   );
